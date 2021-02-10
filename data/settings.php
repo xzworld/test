4d2de4c18034efb00ef52c275de0dcc26856d729
xzworld <75731334+xzworld@users.noreply.github.com>
2021-02-10 03:08:19 +0200

    Update settings.php

--- data/settings.php
+++ data/settings.php
@@ -4,7 +4,7 @@
 $db_host_adress       = 'localhost:3306'; 
 $db_database_name     = 'test';
 $db_database_user     = 'root';  
-$db_database_password = '260386';
+$db_database_password = 'JNgsdewQHY2';
 $db_database_charset  = 'utf8';
 
 
